1485e9744abd722b7515efcbe4823cb8dc887411
Keywind <watersprayer127@gmail.com>
2024-11-18 14:23:05 +0800

    perf: reduce relu overhead

--- src/relu.s
+++ src/relu.s
@@ -25,17 +25,18 @@
 relu:
     li t0, 1             
     blt a1, t0, error     
-    li t1, 0             
+    li t1, 0
+
+    slli t2, a1, 2                      # t2 = a1 * 4 = arr_size * 4
 
 loop_start:
-    beq t1, a1, relu_loop_end           # terminate loop if t1 == a1; index == array_size
-    slli t0, t1, 2                      # t0 = t1 << 2 = t1 * 4
-    add t0, a0, t0                      # t0 = &array + t1 * 4
-    lw t3, 0(t0)                        # t3 = array[t1]
+    beq t1, t2, relu_loop_end           # terminate loop if t1 == t2; index == 4 * array_size
+    add t0, a0, t1                      # t1 = &array + index * 4
+    lw t3, 0(t0)                        # t3 = array[index]
     bgt t3, x0, after_relu_loop_swap    # skip update if greater than zero
     sw x0, 0(t0)                        # array[t1] = 0
 after_relu_loop_swap:
-    addi t1, t1, 1                      # t1 = t1 + 1; index = index + 1
+    addi t1, t1, 4                      # t1 = t1 + 4; index = index + 4
     j loop_start                        # continue loop
 
 relu_loop_end:
